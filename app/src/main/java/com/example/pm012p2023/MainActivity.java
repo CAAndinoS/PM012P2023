19e0e46928d0d50fd7731992e3257596b26ba9d3
CAAndinoS <124225358+CAAndinoS@users.noreply.github.com>
2023-02-22 21:13:31 -0600

    22/2/23

--- app/src/main/java/com/example/pm012p2023/MainActivity.java
+++ app/src/main/java/com/example/pm012p2023/MainActivity.java
@@ -2,7 +2,16 @@ package com.example.pm012p2023;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DownloadManager;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ConsumirApi();
+    }
+    private void ConsumirApi() {
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        String uri = "https://jsonplaceholder.typicode.com/posts";
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, uri, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String s) {
+                Toast.makeText(MainActivity.this, s.substring(0,500), Toast.LENGTH_LONG).show();
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        queue.add(stringRequest);
+
+
     }
 }
