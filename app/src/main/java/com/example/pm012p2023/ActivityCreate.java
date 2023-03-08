9339bf3d4b73110270183589dc7aa2ef2d996d4e
CAAndinoS <124225358+CAAndinoS@users.noreply.github.com>
2023-03-08 10:45:32 -0600

    7/3/23

--- app/src/main/java/com/example/pm012p2023/ActivityCreate.java
+++ app/src/main/java/com/example/pm012p2023/ActivityCreate.java
@@ -1,8 +1,15 @@
 package com.example.pm012p2023;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -19,13 +26,43 @@ import java.util.HashMap;
 
 public class ActivityCreate extends AppCompatActivity {
 
+    ImageView imgaen;
+    Button btngaleria;
+    static final int Result_galeria = 101;
     String POSTMethod;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
 
+        ControlsSet();
+        btngaleria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                GaleriaImagenes();
+            }
+        });
         ConsumeCreateApi();
+    }
+
+    private void GaleriaImagenes() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, Result_galeria);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Uri imageUri;
+        if(resultCode == RESULT_OK && requestCode == Result_galeria){
+            imageUri = data.getData();
+            imgaen.setImageURI(imageUri);
+        }
+    }
+
+    private void ControlsSet() {
+        imgaen = (ImageView) findViewById(R.id.imagen);
+        btngaleria = (Button) findViewById(R.id.btngaleria);
     }
 
     private void ConsumeCreateApi() {
